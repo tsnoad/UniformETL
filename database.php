13bca546e624ed349a0d32544865a5c327500d03
TSnoad <TSnoad@eacbr-db4.(none)>
2011-05-16 11:57:07 +1000

    change database config

--- database.php
+++ database.php
@@ -7,7 +7,7 @@ $conf = New Conf();
 function runq($query) {
 	global $conf;
 
-	$conn = pg_connect("dbname=".$conf->dbname." user=".$conf->user."");
+	$conn = pg_connect("host=".$conf->dbhost." port=5432 dbname=".$conf->dbname." user=".$conf->dbuser." password=".$conf->dbpass."");
 	$result = pg_query($conn, $query);
 	$return = pg_fetch_all($result);
 	pg_close($conn);
