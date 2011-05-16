a4020554b8eeb840b440dc0d57a35083fa37c64c
TSnoad <TSnoad@eacbr-db4.(none)>
2011-05-16 17:46:38 +1000

    api oo wip

--- api/index.php
+++ api/index.php
@@ -34,14 +34,97 @@ function returnXMLData($data) {
 }
 */
 
+/*
+Class API {
+	
+}
+
 Class Users {
 }
 
 Class User {
+	function who_me() {
+		return preg_match("/^users\/[0-9]+\/?$/", $_GET['url']);
+	}
+
+	function what_do_i_do() {
+		switch ($_SERVER['REQUEST_METHOD']) {
+			case "GET":
+				$this->get_user();
+				break;
+			case "POST":
+				$this->update_user();
+				break;
+			case "PUT":
+			case "DELETE":
+			default:
+				header("HTTP/1.1 400 Bad Request");
+				die("HTTP/1.1 400 Bad Request");
+				break;
+		}
+	}
+
+	function get_user() {
+		$member_id = $this->get_member_id();
+
+		//get the member's information
+		$user_query = runq("SELECT m.member_id, w.member_id=m.member_id as web_status, e.member_id=m.member_id as ecpd_status FROM member_ids m LEFT JOIN web_statuses w ON (w.member_id=m.member_id) LEFT JOIN ecpd_statuses e ON (e.member_id=m.member_id) WHERE m.member_id='".pg_escape_string($member_id)."' LIMIT 1;");
+		$user = $user_query[0];
+	
+		//not in database?
+		if (empty($user_query)) {
+			header("HTTP/1.1 404 Not Found");
+			die("HTTP/1.1 404 Not Found");
+		}
+	
+		//get member's names
+		$names_query = runq("SELECT type, given_names, family_name FROM names n WHERE n.member_id='".pg_escape_string($member_id)."';");
+		foreach ($names_query as $names_query_tmp) {
+			//organise names by name type (there's only ever one name per name type)
+			$user['names'][$names_query_tmp['type']] = array("given_names" => $names_query_tmp['given_names'], "family_name" => $names_query_tmp['family_name']);
+		}
+	
+		//get member's email address
+		//ALL email addresses are equal. there is no preffered email address.
+		$emails_query = runq("SELECT email FROM emails e WHERE e.member_id='".pg_escape_string($member_id)."';");
+		foreach ($emails_query as $emails_query_tmp) {
+			//put email addresses in array
+			$user['emails'][] = $emails_query_tmp['email'];
+		}
+	
+		//get member's addresses
+		$address_query = runq("SELECT type, address, suburb, state, postcode, country FROM addresses a WHERE a.member_id='".pg_escape_string($member_id)."';");
+		$user['addresses'] = $address_query;
+	
+		//all good
+		header("HTTP/1.1 200 OK");
+	
+		//return JSON string
+		echo json_encode($user);
+	}
+
+	function update_user() {
+		$member_id = $this->get_member_id();
+	}
+
+	function get_member_id() {
+		//get the member id
+		preg_match("/^users\/([0-9]+)\/?$/", $_GET['url'], &$matches);
+		$member_id = $matches[1];
+	
+		//no member id?
+		if (empty($member_id)) {
+			header("HTTP/1.1 400 Bad Request");
+			die("HTTP/1.1 400 Bad Request");
+		}
+
+		return $member_id;
+	}
 }
 
 Class UserLogin {
 }
+*/
 
 //if the request is for /users
 if (preg_match("/^users\/?$/", $_GET['url'])) {
