849a062b951f1d7f34faaf1694d624cae64fe226
TSnoad <TSnoad@eacbr-db4.(none)>
2011-05-16 11:57:17 +1000

    change database config

--- config.template.php
+++ config.template.php
@@ -2,8 +2,10 @@
 
 class Conf {
 	//database.php
+	public $dbhost = "localhost";
 	public $dbname = "hotel";
-	public $user = "user";
+	public $dbuser = "";
+	public $dbpass = "";
 
 	//extract_launcher.php
 	public $server = "user@remote.server.com";
@@ -11,18 +13,7 @@ class Conf {
 	public $dumps_path = '/data01/datadump/*.tgz';
 	public $dump_path_check_regex = "/^\/data01\/datadump\/FoxtrotTableDump[0-9]{8,8}\.tgz$/";
 
-/*
-	public $identity = "";
-	public $server = "golf@eacbr-db1.nat.internal";
-	public $dumps_path = '/var/golf/foxtrot_dump/*201101*.tgz';
-	public $dump_path_check_regex = "/^\/var\/golf\/foxtrot_dump\/FoxtrotTableDump[0-9]{8,8}\.tgz$/";
-*/
-
 	public $software_path = "/home/user/hotel/";
-
-	//extract.php
-	//where are the dump files
-/* 	public $dump_path = $software_path."extract/extract_processes/"; */
 
 	public $model_path = "/home/user/hotel/transform/transform_models/";
 
