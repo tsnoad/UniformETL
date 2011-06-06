c9a051f66b9835fff648a6d0613261029062ff71
tsnoad <tsnoad@jugular.(none)>
2011-06-06 19:05:53 +1000

    wip database function unit tests

---
+++ tests/DatabaseInsertTest.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once("/etc/uniformetl/database.php");
+
+class DatabaseInsertTest extends PHPUnit_Framework_TestCase {
+	protected function setUp() {
+	}
+
+	protected function tearDown() {
+		runq("DELETE FROM member_ids WHERE member_id='10000000';");
+	}
+
+	public function testSuccess() {
+		$query = runq("INSERT INTO member_ids (member_id) VALUES ('10000000');");
+/* 		$this->assertTrue($query); */
+
+		$query = runq("SELECT * FROM member_ids WHERE member_id='10000000';");
+		$this->assertNotEmpty($query);
+		$this->assertNotEmpty($query[0]);
+		$this->assertEquals("10000000", $query[0]['member_id']);
+	}
+
+	public function testSyntaxError() {
+		try {
+			$query = runq("INSERT INTO member_ids (member_id) VALUES ('wsfgl10000000');");
+		} catch (PHPUnit_Framework_Error $e) {
+			var_dump($query);
+/* 			$this->assertFalse($query); */
+
+			return;
+		}
+
+		$this->fail('An expected exception has not been raised.');
+	}
+
+	public function testDuplicateError() {
+		$query = runq("INSERT INTO member_ids (member_id) VALUES ('10000000');");
+/* 		$this->assertTrue($query); */
+
+		try {
+			$query = runq("INSERT INTO member_ids (member_id) VALUES ('10000000');");
+		} catch (PHPUnit_Framework_Error $e) {
+			var_dump($query);
+/* 			$this->assertFalse($query); */
+
+			return;
+		}
+
+		$this->fail('An expected exception has not been raised.');
+	}
+}
+
+?>
---
+++ tests/DatabaseSelectTest.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("/etc/uniformetl/database.php");
+
+class DatabaseSelectTest extends PHPUnit_Framework_TestCase {
+	protected function setUp() {
+		runq("INSERT INTO member_ids (member_id) VALUES ('10000000');");
+	}
+
+	protected function tearDown() {
+		runq("DELETE FROM member_ids WHERE member_id='10000000';");
+	}
+
+	public function testSuccess() {
+		$query = runq("SELECT * FROM member_ids WHERE member_id='10000000';");
+		$this->assertNotEmpty($query);
+		$this->assertNotEmpty($query[0]);
+		$this->assertEquals("10000000", $query[0]['member_id']);
+	}
+
+	public function testEmpty() {
+		$query = runq("SELECT * FROM member_ids WHERE member_id='10000001';");
+		$this->assertFalse($query);
+	}
+
+	public function testSyntaxError() {
+		try {
+			$query = runq("SELECT * FROM member_ids WHERE quizblorg='10000000';");
+		} catch (PHPUnit_Framework_Error $e) {
+			var_dump($query);
+/* 			$this->assertFalse($query); */
+
+			return;
+		}
+
+		$this->fail('An expected exception has not been raised.');
+	}
+}
+
+?>
