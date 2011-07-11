79370810928e269bdfd133dabede432248f39956
tsnoad <tsnoad@jugular.(none)>
2011-07-11 16:02:09 +1000

    api unit tests

---
+++ tests/APITest.php
@@ -0,0 +1,256 @@
+<?php
+
+require_once("/etc/uniformetl/config.php");
+require_once("/etc/uniformetl/database.php");
+require_once("/etc/uniformetl/transform/transform_models/member_ids.php");
+require_once("/etc/uniformetl/transform/transform_models/member_personals.php");
+require_once("/etc/uniformetl/transform/transform_models/member_names.php");
+require_once("/etc/uniformetl/transform/transform_models/member_emails.php");
+require_once("/etc/uniformetl/transform/transform_models/member_addresses.php");
+require_once("/etc/uniformetl/transform/transform_models/member_passwords.php");
+require_once("/etc/uniformetl/transform/transform_models/member_divisions.php");
+require_once("/etc/uniformetl/transform/transform_models/member_grades.php");
+require_once("/etc/uniformetl/transform/transform_models/member_web_statuses.php");
+require_once("/etc/uniformetl/transform/transform_models/member_ecpd_statuses.php");
+require_once("/etc/uniformetl/transform/transform_models/member_confluence_statuses.php");
+
+class TransformModelsTest extends PHPUnit_Framework_TestCase {
+	protected function setUp() {
+		$this->user_model = new MemberIds;
+		$this->personal_model = new MemberPersonals;
+		$this->name_model = new MemberNames;
+		$this->email_model = new MemberEmails;
+		$this->address_model = new MemberAddresses;
+		$this->password_model = new MemberPasswords;
+		$this->division_model = new MemberDivisions;
+		$this->grade_model = new MemberGrades;
+		$this->web_status_model = new MemberWebStatuses;
+		$this->ecpd_status_model = new MemberEcpdStatuses;
+
+		$this->user_model->add_data("10000000");
+		$this->personal_model->add_data(array("member_id" => "10000000", "gender" => "M", "date_of_birth" => "2011-06-10 13:24:00"));
+		$this->name_model->add_data(array("member_id" => "10000000", "type" => "PREF", "given_names" => "Some", "family_name" => "Name"));
+		$this->email_model->add_data(array("member_id" => "10000000", "email" => "someone@example.com"));
+		$this->address_model->add_data(array("member_id" => "10000000", "type" => "PRIV", "address" => "123 fake st", "suburb" => "somewhere", "state" => "act", "postcode" => "1234", "country" => "AA"));
+		$this->password_model->add_data(array("member_id" => "10000000", "password" => "foobar123"));
+		$this->division_model->add_data(array("member_id" => "10000000", "division" => "Some Division"));
+		$this->grade_model->add_data(array("member_id" => "10000000", "grade" => "Some Grade"));
+		$this->web_status_model->add_data("10000000");
+		$this->ecpd_status_model->add_data("10000000");
+	}
+
+	protected function tearDown() {
+		$this->user_model->delete_data("10000000");
+	}
+
+	public function testGetUserSuccess() {
+		$api_query = shell_exec("curl -s -i -k https://192.168.25.234/api/users/10000000 --get --data \"api_key=".Conf::$api_key."\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 200 OK", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$data = json_decode($response, true);
+
+		$this->assertTrue(is_array($data));
+		$this->assertEquals("10000000", $data['member_id']);
+		$this->assertEquals("M", $data['gender']);
+		$this->assertEquals("2011-06-10 13:24:00", $data['date_of_birth']);
+
+		$this->assertEquals("t", $data['web_status']);
+		$this->assertEquals("t", $data['ecpd_status']);
+		$this->assertEquals("Some Grade", $data['grade']);
+		$this->assertEquals("Some Division", $data['division']);
+
+		$this->assertTrue(is_array($data['names']));
+		$this->assertTrue(is_array($data['names']['PREF']));
+		$this->assertEquals("Some", $data['names']['PREF']['given_names']);
+		$this->assertEquals("Name", $data['names']['PREF']['family_name']);
+
+		$this->assertTrue(is_array($data['emails']));
+		$this->assertEquals("someone@example.com", $data['emails'][0]);
+
+		$this->assertTrue(is_array($data['addresses']));
+		$this->assertTrue(is_array($data['addresses'][0]));
+		$this->assertEquals("PRIV", $data['addresses'][0]['type']);
+		$this->assertEquals("123 fake st", $data['addresses'][0]['address']);
+		$this->assertEquals("somewhere", $data['addresses'][0]['suburb']);
+		$this->assertEquals("act", $data['addresses'][0]['state']);
+		$this->assertEquals("1234", $data['addresses'][0]['postcode']);
+		$this->assertEquals("AA", $data['addresses'][0]['country']);
+	}
+
+	public function testGetUserBadId() {
+		$api_query = shell_exec("curl -s -i -k https://192.168.25.234/api/users/foobar --get --data \"api_key=".Conf::$api_key."\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 400 Bad Request", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 400 Bad Request", $response);
+	}
+
+	public function testGetUserDoesntExist() {
+		$api_query = shell_exec("curl -s -i -k https://192.168.25.234/api/users/10000001 --get --data \"api_key=".Conf::$api_key."\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 404 Not Found", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 404 Not Found", $response);
+	}
+
+	public function testUserUpdateSuccess() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000000 --data \"api_key=".Conf::$api_key."\" --data \"password=somethingdifferent\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 200 OK", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 200 OK", $response);
+
+		$password_query = runq("SELECT * FROM passwords WHERE member_id='10000000';");
+		$this->assertNotEmpty($password_query, "password was not created");
+		$this->assertEquals(md5($password_query[0]['salt']."somethingdifferent"), $password_query[0]['hash'], "password was not set correctly");
+	}
+
+	public function testUserUpdateBadId() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/foobar --data \"api_key=".Conf::$api_key."\" --data \"password=somethingdifferent\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 400 Bad Request", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 400 Bad Request", $response);
+	}
+
+/*
+	public function testUserUpdateDoesntExist() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000001 --data \"api_key=".Conf::$api_key."\" --data \"password=somethingdifferent\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 404 Not Found", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 404 Not Found", $response);
+	}
+*/
+
+	public function testUserLoginSuccess() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000000/login --data \"api_key=".Conf::$api_key."\" --data \"password=foobar123\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 200 OK", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 200 OK", $response);
+	}
+
+	public function testUserLoginBadId() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/foobar/login --data \"api_key=".Conf::$api_key."\" --data \"password=foobar123\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 400 Bad Request", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 400 Bad Request", $response);
+	}
+
+	public function testUserLoginNoPassword() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000000/login --data \"api_key=".Conf::$api_key."\" --data \"password=\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 400 Bad Request", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 400 Bad Request", $response);
+	}
+
+	public function testUserUpdateDoesntExist() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000001/login --data \"api_key=".Conf::$api_key."\" --data \"password=foobar123\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 401 Unauthorized", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 401 Unauthorized", $response);
+	}
+
+	public function testUserUpdateWrongPassword() {
+		$api_query = shell_exec("curl -s -i -k curl https://192.168.25.234/api/users/10000000/login --data \"api_key=".Conf::$api_key."\" --data \"password=somewrongpassword\"");
+
+		//make sure the right HTTP response code was returned
+		$this->assertEquals("HTTP/1.1 401 Unauthorized", substr($api_query, 0, strpos($api_query, "\r\n")));
+
+		//where's the split between the header and the response content
+		$split = strpos($api_query, "\r\n\r\n");
+		$this->assertTrue(is_integer($split));
+		$this->assertTrue($split > 0);
+
+		$response = trim(substr($api_query, $split));
+
+		$this->assertEquals("HTTP/1.1 401 Unauthorized", $response);
+	}
+}
+
+?>
---
+++ transform/transform_models/member_receiptallocations.php
@@ -0,0 +1,97 @@
+<?php
+
+Class MemberReceiptAllocations {
+	function hook_models_required_transforms($data) {
+		return array("MemberIds", "MemberReceipts", "MemberInvoices", "MemberInvoiceItems");
+	}
+	function hook_models_required_tables($data) {
+		return array("dump_%{extract_id}_receiptallocation" => "ReceiptAllocation");
+	}
+	function hook_models_transform_priority($data) {
+		return "tertiary";
+	}
+	function hook_extract_index_sql($data) {
+		return array(
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_invoicebatchid ON dump_%{extract_id}_receiptallocation (cast(invoicebatchid AS BIGINT));",
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_invoicebatchposition ON dump_%{extract_id}_receiptallocation (cast(invoicebatchposition AS BIGINT));",
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_invoiceitemcode ON dump_%{extract_id}_receiptallocation (trim(invoiceitemcode));",
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_invoicesubitemcode ON dump_%{extract_id}_receiptallocation (trim(invoicesubitemcode));",
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_receiptbatchid ON dump_%{extract_id}_receiptallocation (cast(receiptbatchid AS BIGINT));",
+			"CREATE INDEX dump_%{extract_id}_receiptallocation_receiptbatchposition ON dump_%{extract_id}_receiptallocation (cast(receiptbatchposition AS BIGINT));"
+		);
+	}
+
+	function get_src_data($src_member_ids_chunk, $extract_id) {
+		return $this->get_src_members_names($src_member_ids_chunk, $extract_id);
+	}
+
+	function get_dst_data($src_member_ids_chunk) {
+		return $this->get_dst_members_names($src_member_ids_chunk);
+	}
+
+	function get_members_names($member_names_query) {
+		if (empty($member_names_query)) return null;
+
+		foreach ($member_names_query as $member_names_query_tmp) {
+			$member_id = trim($member_names_query_tmp['member_id']);
+
+			$name['member_id'] = $member_id;
+			$name['invoiceitem_id'] = trim($member_names_query_tmp['invoiceitem_id']);
+			$name['receipt_id'] = trim($member_names_query_tmp['receipt_id']);
+
+			$name_hash = md5(implode("", $name));
+
+			$members_names[$member_id][$name_hash] = $name;
+		}
+
+		return $members_names;
+	}
+
+	function get_src_members_names($chunk_id, $extract_id) {
+		$src_member_names_query = runq("
+SELECT DISTINCT 
+r.member_id, 
+r.id as receipt_id, 
+ii.id as invoiceitem_id 
+FROM receipts r 
+INNER JOIN dump_{$extract_id}_receiptallocation ra ON (ra.receiptbatchid::BIGINT=r.batchid AND ra.receiptbatchposition::BIGINT=r.batchposition)
+INNER JOIN invoices i ON (ra.invoicebatchid::BIGINT=i.batchid AND ra.invoicebatchposition::BIGINT=i.batchposition)
+INNER JOIN invoiceitems ii ON (ii.invoice_id=i.id AND ii.itemcode=trim(ra.invoiceitemcode) AND ii.subitemcode=trim(ra.invoicesubitemcode))
+INNER JOIN chunk_member_ids ch ON (ch.member_id=r.member_id) 
+WHERE ch.chunk_id='{$chunk_id}';");
+
+		return $this->get_members_names($src_member_names_query);
+	}
+
+	function get_dst_members_names($chunk_id) {
+		$dst_member_names_query = runq("SELECT DISTINCT r.member_id, ra.receipt_id, ra.invoiceitem_id FROM receipts r INNER JOIN receiptallocations ra ON (ra.receipt_id=r.id) INNER JOIN chunk_member_ids ch ON (ch.member_id=r.member_id) WHERE ch.chunk_id='{$chunk_id}';");
+
+		return $this->get_members_names($dst_member_names_query);
+	}
+
+	function add_data($data_add_item) {
+		runq("INSERT INTO receiptallocations (receipt_id, invoiceitem_id) VALUES ('".pg_escape_string($data_add_item['receipt_id'])."', '".pg_escape_string($data_add_item['invoiceitem_id'])."');");
+	}
+
+	function update_data($data_update_item) {
+		//needs to be coded
+	}
+
+	function delete_data($data_delete_by_member) {
+/*
+		if (empty($data_delete_by_member)) return;
+
+		foreach ($data_delete_by_member as $data_delete_item) {
+			runq("DELETE FROM receipts WHERE member_id='".pg_escape_string($data_delete_item['member_id'])."' AND batch_hash='".pg_escape_string($data_delete_item['batch_hash'])."' AND type='".pg_escape_string($data_delete_item['type'])."' AND status='".pg_escape_string($data_delete_item['status'])."' AND type='".pg_escape_string($data_delete_item['type'])."';");
+		}
+*/
+	}
+
+	function transform($src_data_by_members, $dst_data_by_members) {
+		$transform = New PluralTransforms;
+
+		return $transform->transform($src_data_by_members, $dst_data_by_members);
+	}
+}
+
+?>
