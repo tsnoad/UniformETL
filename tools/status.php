79095c5b3d85bba5a9cdd9be2a44178d96211987
tsnoad <tsnoad@jugular.(none)>
2011-08-23 14:14:35 +1000

    transform launcher logic, tests

--- tools/status.php
+++ tools/status.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 function runq($query) {
 	$conn = pg_connect("host=localhost dbname=hotel user=user password=skoobar");
 	$result = pg_query($conn, $query);
@@ -23,80 +24,176 @@ foreach ($processes as $process_key => $process) {
 	$processes[$process_key]['eps'] = $ps_rows[0];
 }
 
+*/
 
+$log = file_get_contents("/etc/uniformetl/logs/transformlog");
+
+$log_lines = explode("\n", $log);
+
+/* $log_lines = array_map(create_function('$a', 'return substr($a, 0, 80);'), $log_lines); */
+
+$log = implode("\n", array_slice($log_lines, -40, 40));
 
 ?>
 
+<!DOCTYPE html>
 <html>
 	<head>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript"></script>
+<script>
+$(function() {
+	var timer, counter = 0;
+	
+	// Update countdown number every second... and count down
+	timer = setInterval(function() {
+
+		counter ++;
+
+		var minutes = Math.floor(counter / (60*10));
+		var seconds = Math.floor((counter / 10) - (minutes * 60)).toString();
+		var second_fracs = Math.round(counter - (minutes * 60 * 10) - (seconds * 10));
+
+		if (seconds.length < 2) {
+			seconds = "0"+seconds;
+		}
+	
+		$("#countup").html(minutes+":"+seconds+"."+second_fracs);
+
+		$(".progressbar_prog").css("width", (counter / 10)+"px");
+
+		if (counter > 1500) { clearInterval(timer)};
+	}, 100);
+});
+
+$(function() {
+	var timer, counter = 9000;
+	
+	// Update countdown number every second... and count down
+	timer = setInterval(function() {
+
+		counter --;
+
+		var minutes = Math.floor(counter / (60*10));
+		var seconds = Math.floor((counter / 10) - (minutes * 60)).toString();
+		var second_fracs = Math.round(counter - (minutes * 60 * 10) - (seconds * 10));
+
+		if (seconds.length < 2) {
+			seconds = "0"+seconds;
+		}
+	
+		$("#countdown").html(minutes+":"+seconds+"."+second_fracs);
+
+		if (counter == 0) {clearInterval(timer)};
+	}, 100);
+
+	$("#messages pre").scrollTop($("#messages pre")[0].scrollHeight);
+
+	flashtimer = setInterval(function() {
+		$("body").toggleClass("flashon");
+	}, 667);
+});
+</script>
+<style>
+	html, body {
+		padding: 30px;
+		background-color: black;
+		color: #e5fdff;
+		font-family: "lucida grande", Lucinda Grande, Lucinda Sans, Lucinda,tahoma,verdana,arial,sans-serif;
+		font-size: 9pt;
+		font-weight: bold;
+	}
+	h1, h2 {
+		margin: 0px;
+		font-size: 9pt;
+		font-weight: bold;
+	}
+	h1 {
+		color: #727e7f;
+	}
+	.hr {
+		margin: 10px 0px;
+		color: #727e7f;
+	}
+	.progressbar {
+		width: 120px;
+		height: 10px;
+		margin: 10px 0px;
+		border: 2px solid #393f40;
+		border-radius: 5px;
+	}
+	.progressbar_prog {
+		height: 6px;
+		margin: 2px;
+		border-radius: 2.5px;
+		background-color: #acbebf;
+	}
+	#timer_elapsed {
+		margin-top: 20px;
+		color: #727e7f;
+	}
+	#timer_remaining {
+		margin-top: 10px;
+	}
+	#messages {
+		width: 360px;
+		height: 240px;
+		margin: 20px 0px;
+		padding: 4px 8px;
+		border: 2px solid #393f40;
+		border-radius: 2.5px;
+		font-family: monospace;
+		font-size: 7pt;
+	}
+	#messages pre {
+		width: 360px;
+		height: 240px;
+		margin: 0px;
+		overflow: hidden;
+	}
+	.prevetl {
+		margin: 5px 0px;
+	}
+	.prevetl_time {
+		color: #727e7f;
+	}
+	.prevetl_failed {
+		padding: 1px 4px;
+	}
+	body.flashon .prevetl_failed {
+		background-color: #e5fdff;
+		color: black;
+	}
+</style>
 	</head>
-	<body onload="
-window.document.getElementById('spinnerneedle').style.webkitTransform = 'rotate(3600deg)';
-window.document.getElementById('spinnerneedlequad1').style.webkitTransform = 'rotate(-3600deg)';
-window.document.getElementById('spinnerneedlequad2').style.webkitTransform = 'rotate(-3600deg)';
-" style="margin: 0px;">
-		<? if (empty($processes)): ?>
-				<div style="margin: 20px 0px;">
-					<div style="font-family: Helvetica; font-size: 30pt; font-weight: bold; text-align: center;">No Processes</div>
-				</div>
-		<? else: ?>
-			<? foreach ($processes as $process): ?>
-				<div style="height: 40%; position: relative;">
-					<div style="position: relative; margin: 0px 0px; color: #333333; font-family: Helvetica Neue; font-size: 60pt; font-weight: bold; letter-spacing: -4pt; text-align: center;">
-<div style="width: 100%; position: absolute; left: 0px; color: transparent; text-shadow: rgba(255, 255, 255, 0.5) 0px 5px 10px;"><span style="visibility: hidden;">...</span>Processing...</div>
+	<body>
+		<h1>_ Uniform ETL</h1>
 
-<span style="visibility: hidden;">...</span>Processing...</div>
+		<div class="hr">-----</div>
 
-					<div style="margin: 0px 0px; font-family: Helvetica Neue; font-size: 60pt; font-weight: bold; letter-spacing: -4pt; text-align: center;"></div>
+		<h2>ETL in progress:</h2>
 
+		<div class="progressbar">
+			<div class="progressbar_prog" style="width: 46px;"></div>
+		</div>
 
-<!--
-					<div style="width: 80%; margin: 10px auto;">
-						<div style="width: 25%; float: left;">
-							<div style="margin: 0px 10px; padding: 30px 0px 5px 0px; background-color: #4e9a06; color: white; font-family: Helvetica Neue; font-size: 15pt; font-weight: bold; letter-spacing: -1pt; text-align: center;">foobar</div>
-						</div>
-						<div style="width: 25%; float: left;">
-							<div style="margin: 0px 10px; padding: 30px 0px 5px 0px; background-color: #4e9a06; color: white; font-family: Helvetica Neue; font-size: 15pt; font-weight: bold; letter-spacing: -1pt; text-align: center;">foobar</div>
-						</div>
-						<div style="width: 25%; float: left;">
-							<div style="margin: 0px 10px; padding: 30px 0px 5px 0px; background-color: #4e9a06; color: white; font-family: Helvetica Neue; font-size: 15pt; font-weight: bold; letter-spacing: -1pt; text-align: center;">foobar</div>
-						</div>
-						<div style="width: 25%; float: left;">
-							<div style="margin: 0px 10px; padding: 30px 0px 5px 0px; background-color: #4e9a06; color: white; font-family: Helvetica Neue; font-size: 15pt; font-weight: bold; letter-spacing: -1pt; text-align: center;">foobar</div>
-						</div>
-						<div style="clear: both;"></div>
-					</div>
--->
+		<div id="timer_elapsed"><span style="visibility: hidden;">/</span> <span id="countup">0</span> elapsed<!--
+</div>
+		<div id="timer_remaining">
+-->/ <span id="countdown">1500</span> remaining</div>
 
+		<div id="messages">
+			<pre><?= $log ?></pre>
+		</div>
 
-					<div style="width: 100px; height: 100px; position: relative; overflow: hidden; margin: 10px auto; -webkit-border-radius: 50%; -webkit-box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.75);">
-						<div id="spinnerneedle" style="width: 100%; height: 100%; position: absolute; left: 0%; top: 0%; -webkit-transform: rotate(0deg); -webkit-transition: all 30000 linear;">
-
-							<div style="width: 50%; height: 50%; position: absolute; left: 0%; top: 0%; overflow: hidden;">
-								<div id="spinnerneedlequad1" style="width: 200%; height: 200%; position: absolute; left: 0%; top: 0%; background-color: #4e9a06; -webkit-border-radius: 50%; -webkit-box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.75); -webkit-transform: rotate(0deg); -webkit-transition: all 30000 linear;"></div>
-							</div>
-
-							<div style="width: 50%; height: 50%; position: absolute; left: 50%; top: 0%; overflow: hidden;">
-								<div id="spinnerneedlequad2" style="width: 200%; height: 200%; position: absolute; left: -100%; top: 0%; background-color: #4e9a06; -webkit-border-radius: 50%; -webkit-box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.75); -webkit-transform: rotate(0deg); -webkit-transition: all 30000 linear;"></div>
-							</div>
-
-						</div>
-
-						<div style="width: 50%; height: 50%; position: absolute; left: 25%; top: 25%; background-color: white; -webkit-border-radius: 50%; -webkit-box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.75);"></div>
-					</div>
-
-<!-- 					<div style="margin: 10px 0px; font-family: Helvetica; font-size: 15pt; text-align: center;">Started <?= $process['start_date']; ?></div> -->
-<!-- 					<div style="margin: 10px 0px; font-family: Helvetica; font-size: 10pt; text-align: center;">With Data From <?= $process['source_timestamp']; ?></div> -->
-					<div style="margin: 10px 0px; font-family: Helvetica; font-size: 15pt; text-align: center;">Process <?= $process['pid']; ?></div>
-					<div style="margin: 10px 0px; font-family: Helvetica; font-size: 15pt; text-align: center;">Process <?= $process['wps']; ?></div>
-					<div style="margin: 10px 0px; font-family: Helvetica; font-size: 15pt; text-align: center;">Process <?= $process['eps']; ?></div>
-
-
-					<div style="width: 100%; height: 10px; position: absolute; left: 0px; bottom: 0px; background-color: #888a85; -webkit-box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.75);"></div>
-				</div>
-
-			<? endforeach; ?>
-		<? endif; ?>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span> <span class="prevetl_failed">FAILED</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span> <span class="prevetl_failed">FAILED</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span> <span class="prevetl_failed">FAILED</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
+		<div class="prevetl">2011-08-18 <span class="prevetl_time">23:45:30</span></div>
 	</body>
 </html>
